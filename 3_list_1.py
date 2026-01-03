f3da57ae4ac87897dc926cf4b8c3d87e7e563e43
Tushar Solanki <solankitushar1217@gmail.com>
2026-01-03 12:01:12 +0530

    Add files via upload

---
+++ 3_list_1.py
@@ -0,0 +1,10 @@
+fav_des =['Gujarat', 'Maharashtra', 'Rajasthan', 'Punjab', 'Haryana']
+list = ['Hello', 123, 45.67, True, 345,'World',23.32,'python',212,22.12]
+
+print("Favorite Destinations:",fav_des)
+print("Values : ",list)
+print("First Value : ",list[0])
+print("First Five Values : ",list[0:5])
+print("Last Five Values : ",list[5:10])
+print(list*2)
+print(list[1:6])
