2af02de61257539b73dc081460c4fd77f311ff65
Tushar Solanki <solankitushar1217@gmail.com>
2026-01-04 19:16:50 +0530

    Add files via upload

---
+++ 1_print.py
@@ -0,0 +1,10 @@
+name = "Tushar"
+address = "Somnath Precidency,Air port road,Bhavnagar"
+city = "Bhavnagar"
+pincode = 364001
+age = 19
+weight = 49
+gender = "Male"
+
+print("Name is ",name,"Address is ",address,"City is ",city,"Pincode is ",pincode,
+ "Age is ",age,"Weight is ",weight,"Gender is ",gender)
---
+++ 2_input.py
@@ -0,0 +1,6 @@
+college_name = input("What is Your College Name ? :")
+branch = input("What is Your Branch ? : ")
+semester = input("In Which Semester You Study ? : ");
+hobbies = input("What are your Hobbies? : ")
+
+print("College Name is ",college_name,"Branch is ",branch,"Semester is ",semester,"Hobbies Are ",hobbies)
