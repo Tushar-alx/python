47cbb90e7d6657568d43533b75fb0cd73399e0aa
Tushar Solanki <solankitushar1217@gmail.com>
2026-01-13 22:30:40 +0530

    Add files via upload

---
+++ 66_extra_3.py
@@ -0,0 +1,8 @@
+#Program to calculate perimeter of circle
+
+radius = int(input("Enter Value of radius : "))
+
+perimeter_circle = 2 * 3.14 * radius
+
+print("Perimeter of Circle : ",perimeter_circle)
+
