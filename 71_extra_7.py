284df5e11e36abf35786cce4ad103e87705109b8
Tushar Solanki <solankitushar1217@gmail.com>
2026-01-18 19:15:01 +0530

    Add files via upload

---
+++ 71_extra_7.py
@@ -0,0 +1,10 @@
+# Program to find number is positive or negative or zero
+
+num = int(input("Enter Number : ")) 
+ 
+if num > 0:
+    print(num, "is a Positive Number")
+if num < 0: 
+    print(num, "is a Negative Number")
+if num == 0:
+    print("Number is Zero")
