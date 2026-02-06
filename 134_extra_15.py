45767b3cc5ebfee607ecdaf6e3e9c15289a60570
Tushar Solanki <solankitushar1217@gmail.com>
2026-02-07 05:21:13 +0530

    Add files via upload

---
+++ 134_extra_15.py
@@ -0,0 +1,15 @@
+'''
+*   *
+ * *
+  *
+ * *
+*   *
+'''
+n = int(input("Enter the size of the pattern: "))
+for i in range(n):
+    for j in range(n):
+        if i == j or i + j == n - 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
